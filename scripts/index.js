4c93ecfebb4bb3a4a13ca76b20990c9f2d970b6d
Federico Pérez <fedemartinp@gmail.com>
2021-02-16 15:16:50 -0300

    Se agregan estilos hover a links y al boton del form.

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,40 @@
 // Vars
 var orangeArrow = 'assets/img/arrow_short_og-min.png';
 var whiteArrow = 'assets/img/arrow_short_wt-min.png';
-var flag = true;
+var flagBg = true;
+var flagBtn = true;
 
 // DOM Elements
 var bgimageOnHover = $('#bgimageOnHover');
 var srcToChange = $('#srcToChange');
 
+var btnOrange = $('#btnOrange');
+var btnArrowImg = $('#btnArrowImg');
+
 // Event Listeners
 bgimageOnHover.on('mouseover', changeSrc);
 bgimageOnHover.on('mouseleave', changeSrc);
 
-// Event function
+btnOrange.on('mouseover', changeArrow);
+btnOrange.on('mouseleave', changeArrow);
+
+// Event Functions
 function changeSrc() {
-    if (flag) {
+    if (flagBg) {
         srcToChange[0].src = orangeArrow;
-        flag = false;
+        flagBg = false;
     } else {
         srcToChange[0].src = whiteArrow;
-        flag = true;
+        flagBg = true;
+    }
+}
+
+function changeArrow() {
+    if (flagBtn) {
+        btnArrowImg[0].src = whiteArrow;
+        flagBtn = false;
+    } else {
+        btnArrowImg[0].src = orangeArrow;
+        flagBtn = true;
     }
 }
