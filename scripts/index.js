9866bd2d58d711e1e1622428719125b9187f4ffd
Federico Pérez <fedemartinp@gmail.com>
2021-02-17 19:25:24 -0300

    Updates de requerimientos.

--- scripts/index.js
+++ scripts/index.js
@@ -1,46 +1,64 @@
-// Vars
-var orangeArrow = 'assets/img/arrow_short_og-min.png';
-var whiteArrow = 'assets/img/arrow_short_wt-min.png';
-var flagBg = true;
-var flagBtn = true;
+$(document).ready(function() {
+    // Vars
+    var orangeArrow = 'assets/img/arrow_short_og-min.png';
+    var whiteArrow = 'assets/img/arrow_short_wt-min.png';
+    var flagBg = true;
+    var flagBtn = true;
 
-// DOM Elements
-var bgimageOnHover = $('#bgimageOnHover');
-var srcToChange = $('#srcToChange');
+    // DOM Elements
+    var bgimageOnHover = $('#bgimageOnHover');
+    var srcToChange = $('#srcToChange');
 
-var btnOrange = $('#btnOrange');
-var btnArrowImg = $('#btnArrowImg');
+    var btnOrange = $('#btnOrange');
+    var btnArrowImg = $('#btnArrowImg');
 
-// Event Listeners
-bgimageOnHover.on('mouseover', changeSrcOver);
-bgimageOnHover.on('mouseleave', changeSrcLeave);
+    var scrollLink = $('#scrollLink');
 
-btnOrange.on('mouseover', changeArrowOver);
-btnOrange.on('mouseleave', changeArrowLeave);
+    // Event Listeners
+    bgimageOnHover.on('mouseover', changeSrcOver);
+    bgimageOnHover.on('mouseleave', changeSrcLeave);
 
-// Event Functions
-function changeSrcOver() {
-    if (flagBg) {
-        srcToChange[0].src = orangeArrow;
-        flagBg = false;
+    btnOrange.on('mouseover', changeArrowOver);
+    btnOrange.on('mouseleave', changeArrowLeave);
+
+    scrollLink.on('click', smoothEffect);
+
+    // Event Functions
+    function changeSrcOver() {
+        if (flagBg) {
+            srcToChange[0].src = orangeArrow;
+            flagBg = false;
+        }
     }
-}
-function changeSrcLeave() {
-    if (!flagBg) {
-        srcToChange[0].src = whiteArrow;
-        flagBg = true;
+    function changeSrcLeave() {
+        if (!flagBg) {
+            srcToChange[0].src = whiteArrow;
+            flagBg = true;
+        }
     }
-}
 
-function changeArrowOver() {
-    if (flagBtn) {
-        btnArrowImg[0].src = whiteArrow;
-        flagBtn = false;
+    function changeArrowOver() {
+        if (flagBtn) {
+            btnArrowImg[0].src = whiteArrow;
+            flagBtn = false;
+        }
     }
-}
-function changeArrowLeave() {
-    if (!flagBtn) {
-        btnArrowImg[0].src = orangeArrow;
-        flagBtn = true;
+    function changeArrowLeave() {
+        if (!flagBtn) {
+            btnArrowImg[0].src = orangeArrow;
+            flagBtn = true;
+        }
     }
-}
+
+    function smoothEffect(e) {
+        e.preventDefault();
+        if (this.hash !== "") {
+        var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function() {
+                window.location.hash = hash;
+            });
+        }
+    }
+});
